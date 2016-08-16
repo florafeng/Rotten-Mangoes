3b0d374ab95214de7dc37f15a246ac61d17af2a9
florafeng <sfeng3@ualberta.ca>
2016-08-16 22:58:17 +0000

    adding links to log in, sign up, or log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/destroy'
+
   get 'sessions/new'
 
   get 'sessions/create'
@@ -7,5 +9,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
